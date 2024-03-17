f1f19b2bd6a8c43bc4491bef8ca5d5ae70b19aa4
김범중 <rlaqjawnd625@naver.com>
2024-03-18 00:33:57 +0900

    feat: implement User Entity

---
+++ backend/src/main/java/klog/blog_project/entity/User.java
@@ -0,0 +1,63 @@
+package klog.blog_project.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "permission_id")
+    private Permission permission;
+
+    private String id;
+
+    private String password;
+
+    private String nickname;
+
+    @Column(name = "profile_image")
+    private String profileImage;
+
+    @Column(name = "introduction_title")
+    private String introductionTitle;
+
+    @Column(name = "introduction_content")
+    private String introductionContent;
+
+    @Builder
+    public User(String id, String password, String nickname) {
+        this.permission = Permission.Member;
+        this.id = id;
+        this.password = password;
+        this.nickname = nickname;
+        this.profileImage = "https://avatars.githubusercontent.com/u/121084350?v=4";
+        this.introductionTitle = "안녕하세요";
+        this.introductionContent = "반갑습니다";
+    }
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "user")
+    private List<Post> posts = new ArrayList<>();
+}
