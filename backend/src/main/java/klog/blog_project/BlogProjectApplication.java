feb00e4f1fefdea1480c7b94aa122f7bdde0eea5
김범중 <rlaqjawnd625@naver.com>
2024-03-18 00:33:20 +0900

    feat: implement BaseTimeEntity for tracking creation and modification dates across entities & add auditing annotation at Application

--- backend/src/main/java/klog/blog_project/BlogProjectApplication.java
+++ backend/src/main/java/klog/blog_project/BlogProjectApplication.java
@@ -2,12 +2,14 @@ package klog.blog_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BlogProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BlogProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BlogProjectApplication.class, args);
+    }
 
 }
